c11ce67e305aad229243b30ac0e835ff0156b53c
Devin Kenney <devin.kenney111@gmail.com>
2023-02-21 10:05:29 -0500

    Week 9 Day 1

--- react-tac-toe/src/App.js
+++ react-tac-toe/src/App.js
@@ -1,26 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
 
+  const [counter, setCounter] = useState(0)
+  const [evenCounter, setEvenCounter] = useState(0)
 
+  useEffect(() => {
+    const effect = () => {
+      console.log("I'm just a random log")
+    }
+    effect()
+  }, [evenCounter])
+
+  const addOne = () => {
+    setCounter(counter + 1)
+
+    if (counter % 2 === 0) {
+      setEvenCounter(evenCounter + 1)
+    }
+  }
+
+  const colors = [
+    {
+      name: "blue",
+      type: "cool"
+    },
+    {
+      name: "red",
+      type: "warm"
+    },
+    {
+      name: "orange",
+      type: "warm"
+    },
+    {
+      name: "green",
+      type: "cool"
+    }
+  ]
+
+  const wise = {
+    difficulty: "hard",
+    length: "18 weeks",
+    languageLearned: "JavaScript"
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {colors.map((color) => {
+        return (
+          <div>
+            Name: {color.name}
+            <br />
+            Type: {color.type}
+            <br />
+          </div>
+        )
+      })}
+
+      <h2>WISE info</h2>
+
+      {Object.keys(wise).map((key) => {
+        return (
+          <div>
+            {key}: {wise[key]}
+          </div>
+        )
+      })}
+
+      <h2>Counter:</h2>
+      <h3>{counter}</h3>
+      <h2>Even Counter:</h2>
+      <h3>{evenCounter}</h3>
+      <button onClick={addOne}>Click me to add one to the counter!</button>
+
     </div>
   );
 }
