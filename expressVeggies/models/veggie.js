b1343d10403206b95d22e7ee0dcb70f03bb56193
Devin Kenney <devin.kenney111@gmail.com>
2023-04-05 11:07:47 -0400

    week 14 day 3 ish

---
+++ expressVeggies/models/veggie.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const veggiesSchema = new Schema({
+  name: String,
+  color: String,
+  readyToEat: Boolean
+});
+
+const Veggie = model('Veggie', veggiesSchema);
+
+module.exports = Veggie;
--- expressVeggies/server.js
+++ expressVeggies/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const db = mongoose.connection;
 
 // SETTING THE VIEW ENGINE
 app.set('view engine', 'jsx');
@@ -14,15 +15,16 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-mongoose.connection.once('open', () => {
-  console.log('connected to mongo!');
-});
+db.on('error', err => console.log(err.message + ' is mongod not running?'));
+db.on('open', () => console.log('mongo connected: ', mongoURI));
+db.on('close', () => console.log('mongo disconnected!'))
 
 // MIDDLEWARE
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }))
 
-
+const veggieController = require('./controllers/veggies');
+app.use('/veggies', veggieController)
 
 // LISTEN
 app.listen(3000, () => {
